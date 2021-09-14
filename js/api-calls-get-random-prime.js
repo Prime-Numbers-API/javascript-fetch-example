4c26f5ae72b5aa2ab55571b3f667259dc4c00c43
Marius Banea <marius@w-d-s.co.uk>
2021-09-14 15:24:42 +0100

    change from ajax to fetch

--- js/api-calls-get-random-prime.js
+++ js/api-calls-get-random-prime.js
@@ -31,21 +31,19 @@ $(document).ready(function () {
 
         console.log(get_random_prime_api_url)
 
-        $.ajax({
-            url: get_random_prime_api_url,
-            type: 'get',
-            cache: false,
-            success: function (incomingData) {
-                // console.log('success!');
-                get_random_prime_api_results(incomingData);
-            },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
-                console.log("Status: ", textStatus); 
-                console.log("Error: ", errorThrown);
-                console.log("XMLHttpRequest: ", XMLHttpRequest.responseJSON.error);
-                displayError(XMLHttpRequest.responseJSON.error, "error-get-random-prime")
-            }
-        });
+
+        fetch(get_random_prime_api_url)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error(response.statusText);
+            })
+            .then(responseJson => get_random_prime_api_results(responseJson))
+            .catch(err => {
+                console.log(err);
+                displayError(err.error, "error-get-random-prime")
+            });
     };
 })
 
